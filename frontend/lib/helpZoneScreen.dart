ad7c496ebe453d8a469ce3d4d22852f3f3f0d450
EyderACM <eyderacm@gmail.com>
2019-08-25 05:58:13 -0500

    cambios de eyder

---
+++ frontend/lib/helpZoneScreen.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:frontend/helpZoneScreen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:frontend/listPlaces.dart';
+
+class HelpZone extends StatefulWidget {
+  HelpZone({Key key}) : super(key: key);
+
+  _HelpZoneState createState() => _HelpZoneState();
+}
+
+class _HelpZoneState extends State<HelpZone> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: Container(
+            child: Column(
+      children: <Widget>[],
+    )));
+  }
+}
