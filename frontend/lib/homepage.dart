157963a802103b18bc3315d95b4c63f3717d0172
jefApr <jef.apr.99@gmail.com>
2019-08-25 02:18:04 -0500

    comienzo vista de list  de centros de acopio

--- frontend/lib/homepage.dart
+++ frontend/lib/homepage.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter/material.dart';
-
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState()=>HomePageState();
